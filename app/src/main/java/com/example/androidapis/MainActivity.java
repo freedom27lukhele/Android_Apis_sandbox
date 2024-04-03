5cd14b37ab7488cb31dcc7fa4f50625b4afb0c06
FreedomL <freedoml@blts.co.za>
2024-04-03 09:05:53 +0200

    adding the second interation of RetrofitApi calls

--- app/src/main/java/com/example/androidapis/MainActivity.java
+++ app/src/main/java/com/example/androidapis/MainActivity.java
@@ -66,33 +66,26 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<DataModal>() {
             @Override
             public void onResponse(Call<DataModal> call, Response<DataModal> response) {
-                // this method is called when we get response from our api.
+
                 Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
 
-                // below line is for hiding our progress bar.
                 loadingPB.setVisibility(View.GONE);
 
-                // on below line we are setting empty text
-                // to our both edit text.
                 edtJob.setText("");
                 edtName.setText("");
 
-                // we are getting response from our body
-                // and passing it to our modal class.
                 DataModal responseFromAPI = response.body();
 
-                // on below line we are getting our data from modal class and adding it to our string.
+
                 String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getName() + "\n" + "Job : " + responseFromAPI.getJob();
 
-                // below line we are setting our
-                // string to our text view.
                 tvResponse.setText(responseString);
             }
+
             @Override
             public void onFailure(Call<DataModal> call, Throwable t) {
-                // setting text to our text view when
-                // we get error response from API.
-                tvResponse.setText("Error found is : " + t.getMessage());
+
+                tvResponse.setText(String.format("%s%s", getString(R.string.error_found_is), t.getMessage()));
             }
         });
     }
